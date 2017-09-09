7357bd6298be19d47679449403acfc8cbf5ff5a1
Hayato_T <tsunoda.gameobea@x.gmobb.jp>
2017-09-09 21:04:54 +0900

    set routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/user/new', to: 'users#new'
+  post '/matching', to: 'matchings#add'
+  get '/matching', to: 'matchings#ismatched'
+  post '/feeling', to: 'feelings#add'
 end
